33169b6764ee6d5b96a0a8681935e7ae80a9a55e
Srikanthchow <kilarisrikanthchowdary@gmail.com>
2024-12-20 09:19:33 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './components/Home'
+import Movies from './components/Movies'
+
+export default function App() {
+  return (
+    <div>
+
+      <Movies />
+    </div>
+  )
+}
